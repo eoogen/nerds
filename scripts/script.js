0a060cdadd14cc2ac69109701e152a7c5dfc868a
eoogen <jangootoo@gmail.com>
2018-12-27 16:34:48 +0200

    MODAL_changes

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ var link = document.querySelector(".button-contacts");
 var popup = document.querySelector(".modal-contact");
 var close = popup.querySelector(".modal-close");
 var login = popup.querySelector("[id=name-form]");
+var form = popup.querySelector("form");
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -12,4 +13,12 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
 });
