817cfa8de9db8fa9510bb7a7f8a754d364cc023c
eoogen <jangootoo@gmail.com>
2018-12-31 14:59:51 +0200

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
-var link = document.querySelector(".button-contacts");
-var popup = document.querySelector(".modal-contact");
-var close = popup.querySelector(".modal-close");
-var login = popup.querySelector("[id=name-form]");
-var form = popup.querySelector("form");
+var link = document.querySelector('.button-contacts');
+var popup = document.querySelector('.modal-contact');
+var close = popup.querySelector('.modal-close');
+var login = popup.querySelector('[id=name-form]');
+var form = popup.querySelector('form');
 
-link.addEventListener("click", function (evt) {
+link.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add("modal-show");
+    popup.classList.add('modal-show');
     login.focus();
 });
 
-close.addEventListener("click", function (evt) {
+close.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.remove("modal-show");
+    popup.classList.remove('modal-show');
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
-        if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
+        if (popup.classList.contains('modal-show')) {
+            popup.classList.remove('modal-show');
         }
     }
 });
